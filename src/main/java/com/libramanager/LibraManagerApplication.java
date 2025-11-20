693bd7b8c156641a72991c610bb4ce92768d9ecb
mohamedamid <amid.mohamed2024@gmail.com>
2025-11-20 22:23:54 +0100

    first commit

---
+++ src/main/java/com/libramanager/LibraManagerApplication.java
@@ -0,0 +1,13 @@
+package com.libramanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/libramanager/LibraManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libramanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
